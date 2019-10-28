d4de2dc62aa43eaced42387c7dfa2244cfc8e2f2
Atsushi <>
2019-10-28 22:18:43 +0900

    first commit

---
+++ SwiftFirstApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftFirstApp
+//
+//  Created by Atsushi on 2019/10/28.
+//  Copyright © 2019 Atsushi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
